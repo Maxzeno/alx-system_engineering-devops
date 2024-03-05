ef75e767ddf53b523d25d82fc5fa0029209789b2
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-03-05 19:13:52 +0100

    changed the dot format f string

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,7 +13,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         after (str): The parameter for the next page of the API results.
         count (int): The parameter of results matched thus far.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
